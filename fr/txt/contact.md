5de0abbd47718cdc7a9dc4fb650599eb409838d7
Hamed <freeze2ice@gmail.com>
2016-07-10 18:49:49 +0200

    Phase1 final release (without device css support)

---
+++ fr/txt/contact.md
@@ -0,0 +1,6 @@
+Nous écrire ou visiter:
+__Unit 8, 2<sup>nd</sup> Floor, No.1908, Zandie Bldg., Vali Asr Ave., Tehran - IRAN__
+Code postal: __1415884943__
+Email: __bmpinst@aol.com__
+Tél.: __+98 21 88 91 01 56-7__
+Téléfax: __+98 21 88 92 67 90__
--- js/app.js
+++ js/app.js
@@ -9,3 +9,21 @@ $('a[href^=#]').on("click",function(){
         return false;
     }
 });
+
+$(document).ready(function(){
+    $('#sect1 article').load('txt/about.md', function(data){
+        $(this).html(marked(data));
+    });
+    $('#sect2 article').load('txt/visa.md', function(data){
+        $(this).html(marked(data));
+    });
+    $('#sect3 article').load('txt/services.md', function(data){
+        $(this).html(marked(data));
+    });
+    $('#sect4 article').load('txt/team.md', function(data){
+        $(this).html(marked(data));
+    });
+    $('#sect5 article').load('txt/contact.md', function(data){
+        $(this).html(marked(data));
+    });
+});
